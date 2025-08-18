34eec4a9bb18b052b30fe91b5747be169ede678d
nandonellospazio <61141061+nandonellospazio@users.noreply.github.com>
2025-08-18 16:39:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+import webapp as FE
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +7,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    FE.main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
